8a97a733fc676b2fd79e9c0a021aca501e8298bf
Edoriban <edoriban@gmail.com>
2025-08-27 19:50:24 -0600

    feat: Agregar video de fondo y datos de servicios para la sección Vision

---
+++ src/utils/vision-data.ts
@@ -0,0 +1,41 @@
+const services = [
+    {
+        title: "Desarrollo de Software y Automatización",
+        description: "Te ayudamos a digitalizar tus procesos, automatizar flujos y poner en marcha plataformas clave para tu negocio..",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M12 3.75v16.5m8.25-8.25H3.75" /> </svg>`,
+        image: "/images/software1.webp",
+        imageSecondary: "/images/software2.webp",
+        features: [
+            "Aplicaciones web, móviles y desktop",
+            "Automatización de flujos entre sistemas mediante APIs y conectores",
+            "Implementación técnica de plataformas empresariales (CRM, ERP, e-commerce)"
+        ]
+    },
+    {
+        title: "Trazabilidad, Monitoreo de Producción",
+        description: "Digitalizamos procesos industriales para obtener trazabilidad en tiempo real, visualizar indicadores operativos y mejorar la toma de decisiones.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v18m9-9H3" /> </svg>`,
+        image: "/images/tracking1.webp",
+        imageSecondary: "/images/tracking2.webp",
+        features: [
+            "Sistemas MES, ANDON y OEE",
+            "Dashboards personalizados para áreas operativas",
+            "Integración con PLCs, sensores e IoT"
+        ]
+    },
+    {
+        title: "Visión con Inteligencia Artificial",
+        description: "Aplicamos tecnologías de visión computacional e inteligencia artificial para automatizar inspecciones, clasificaciones y procesos críticos en entornos industriales.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M12 3.75v16.5m8.25-8.25H3.75" /> </svg>`,
+        image: "/images/vision1.webp",
+        imageSecondary: "/images/vision2.webp",
+        features: [
+            "Sistemas de visión para inspección de calidad",
+            "Reconocimiento de patrones y objetos",
+            "Clasificación automática mediante IA",
+            "Desarrollo de modelos personalizados de machine learning"
+        ]
+    },
+];
+
+export { services };
