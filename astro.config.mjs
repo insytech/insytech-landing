33b6cd72cd26d9037cc4ccdcd74386ce77da51f9
Edoriban <edoriban@gmail.com>
2025-12-18 13:25:23 -0600

    Mejoras en SEO e implementación de google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://insytech.mx',
     output: 'static',
     adapter: vercel(),
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [react()],
+    integrations: [react(), sitemap()],
 });
