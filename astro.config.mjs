1b972044721f8407e379c8ad2ca40fb9521117ad
Edoriban <edoriban@gmail.com>
2025-12-17 18:50:14 -0600

    perf: switch to static output with robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercelServerless({
-        edgeMiddleware: true,
-    }),
+    output: 'static',
+    adapter: vercel(),
     vite: {
         plugins: [tailwindcss()],
     },
